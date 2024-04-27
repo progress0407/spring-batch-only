cc540f33f49ed6504cf709dd221facebc76cb4fb
progress0407 <progress0407@gmail.com>
2024-04-27 20:00:19 +0900

    init spring batch project

---
+++ src/main/java/io/philo/batchlab/BatchLabApplication.java
@@ -0,0 +1,13 @@
+package io.philo.batchlab;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BatchLabApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BatchLabApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/philo/batchlab/BatchLabApplicationTests.java
@@ -0,0 +1,13 @@
+package io.philo.batchlab;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BatchLabApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
